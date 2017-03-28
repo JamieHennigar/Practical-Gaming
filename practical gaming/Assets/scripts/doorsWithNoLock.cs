110c810f44d35c62cd2d40168c652d15123cd80f
JamieHennigar <jamiehennigar@gmail.com>
2017-03-28 10:55:48 +0100

    made script to open doors that have no locks and started placing all of the door prefabs.

---
+++ practical gaming/Assets/scripts/doorsWithNoLock.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorsWithNoLock : MonoBehaviour {
+
+    public GameObject door;
+    Animator doorAnimator;
+    bool doorOpen = false;
+   
+    // Use this for initialization
+    void Start () {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            openDoors();
+
+        }
+    }
+
+
+
+
+
+
+
+    private void openDoors()
+    {
+        Ray mousePointer = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(mousePointer, out hit))
+        {
+            if (Vector3.Distance(transform.position, door.transform.position) < 5)
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    print("Open Sesame");
+                    doorAnimator.SetBool("open", true);
+
+
+                    doorOpen = true;
+                }
+            }
+        }
+        /*else if (Vector3.Distance(transform.position, door.transform.position) < 5 && (doorOpen = true))
+        {
+            doorAnimator.Play("Door_Close");
+            doorOpen = false;
+        }
+        else
+            doorOpen = false;*/
+    }
+
+    /* private void closeDoors()
+     {
+         if (Vector3.Distance(transform.position, door.transform.position) < 5  &&(doorOpen = true ))
+         {
+             doorAnimator.Play("Door_Close");
+             doorOpen = false;
+         }
+     }*/
+
+
+}
--- practical gaming/Assets/scripts/lockedDoorsManager.cs
+++ practical gaming/Assets/scripts/lockedDoorsManager.cs
@@ -53,6 +53,7 @@ public class lockedDoorsManager : MonoBehaviour {
 
                 }
             }
+            
         }
     }
 
